998eb8e101986cd7e36390e6743e7834c2956665
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-08-26 21:45:04 +0300

    Maven project. All tests done.

---
+++ Anagram/src/main/java/com/foxminded/anagram/Anagram.java
@@ -0,0 +1,38 @@
+package com.foxminded.anagram;
+
+import java.util.StringJoiner;
+
+public class Anagram {
+	public String process(String string) {
+		if (string == null) {
+			throw new IllegalArgumentException("Text cannot be null");
+		}
+		StringJoiner joiner = new StringJoiner(" ");
+		for(String word:string.split(" ")) {
+			String reversedWord = new String(reverse(word));
+			joiner.add(reversedWord);
+		}
+		return joiner.toString();
+	}
+	
+	public char [] reverse(String word) {
+		char [] chars = new char [word.length()];
+		int leftIndex = 0;
+		int rightIndex = word.length()-1;
+		while (leftIndex <= rightIndex) {
+			if(!Character.isLetter(word.charAt(leftIndex))&Character.isLetter(word.charAt(rightIndex))){
+				chars[leftIndex]= word.charAt(leftIndex);
+				leftIndex++;
+			}else if(!Character.isLetter(word.charAt(rightIndex))&Character.isLetter(word.charAt(leftIndex))){
+				chars[rightIndex]= word.charAt(rightIndex);
+				rightIndex--;
+			}else if (Character.isLetter(word.charAt(leftIndex))&Character.isLetter(word.charAt(rightIndex))) {
+				chars[leftIndex] = word.charAt(rightIndex);
+				chars[rightIndex] = word.charAt(leftIndex);
+				leftIndex++;
+				rightIndex--;
+			}
+		}	
+		return chars;
+	}
+}
