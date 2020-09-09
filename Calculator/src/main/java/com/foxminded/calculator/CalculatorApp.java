8aed2b187e54168214b7631553cf5f5a31c75459
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-09-09 11:25:11 +0300

    result and step created

--- Calculator/src/main/java/com/foxminded/calculator/CalculatorApp.java
+++ Calculator/src/main/java/com/foxminded/calculator/CalculatorApp.java
@@ -1,14 +1,20 @@
 package com.foxminded.calculator;
 
-import java.util.Scanner;
+import java.util.List;
 
 public class CalculatorApp {
-	public static void main(String [] args) {
-		try (Scanner scanner = new Scanner (System.in)){
-			Calculator calculator = new Calculator();
-			String divident = scanner.nextLine();
-			String divisor = scanner.nextLine();
-			System.out.println(calculator.process(divident,divisor));
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		Result result = calculator.process(343, 3);
+		
+		System.out.println(result.getDividend());
+		List<Step> steps = calculator.process(343, 3).getSteps();
+		Step firstLocalDividend = steps.get(0);
+
+		for (int i = 0; i<steps.size(); i++) {
+
+			System.out.println(steps.get(i).getLocalDividend());
+			System.out.println(steps.get(i).getIntermediate());
 		}
 	}
 }
