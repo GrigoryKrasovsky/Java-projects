3cc8b0f58de9467f7916a88608bc5ca7afd0128d
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-09-15 16:08:33 +0300

    all prints deleted

--- Calculator/src/main/java/com/foxminded/calculator/CalculatorApp.java
+++ Calculator/src/main/java/com/foxminded/calculator/CalculatorApp.java
@@ -11,11 +11,6 @@ public class CalculatorApp {
 		Result result = Calculator.process(-225, 15);
 		Formatter format = FormatterFactory.get("Classic");
 		System.out.println(format.format(result));
-		//System.out.println(Calculator.findDigit(200,0));
-		//System.out.println(result.getAllValues());
-		//System.out.println(Calculator.findLength(38471));
-		//System.out.println(result.getAllValues());
-		
 	}
 
 }
