2fa32a26d1819f8463877d797e7c900bcde8f642
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-09-16 10:41:10 +0300

    Calculator edited

--- Calculator/src/main/java/com/foxminded/calculator/Calculator.java
+++ Calculator/src/main/java/com/foxminded/calculator/Calculator.java
@@ -1,80 +1,74 @@
 package com.foxminded.calculator;
 
-import java.util.Arrays;
-import java.util.StringJoiner;
+import java.util.ArrayList;
+import java.util.List;
+import com.foxminded.calculator.model.Result;
+import com.foxminded.calculator.model.Step;
 
 public class Calculator {
-	public String process(String divident, String divisor) {
-		for (int index = 0; index < divident.length(); index++) {
-			if (!Character.isDigit(divident.charAt(index))) {
-				throw new IllegalArgumentException("Only numbers can be processed");
-			}
+	public static Result process(int dividend, int divisor) {
+		if (divisor == 0) {
+			throw new IllegalArgumentException("Divisor cannot be zero");
 		}
-		for (int index = 0; index < divisor.length(); index++) {
-			if (!Character.isDigit(divisor.charAt(index))) {
-				throw new IllegalArgumentException("Only numbers can be processed");
-			}
+		if((int)Math.abs(dividend) < (int)Math.abs(divisor)) {
+			Step stepp = new Step(0,0);
+			List<Step>stepss = new ArrayList<>();
+			stepss.add(stepp);
+			return new Result(dividend,divisor,dividend/divisor,dividend%divisor,stepss);
 		}
-		
-		return divide(divident,divisor);
-	}
-	public String divide (String divident, String divisor) {
-		
-		StringBuilder builder = new StringBuilder("");
-		StringJoiner joiner = new StringJoiner("");
-		if(Integer.parseInt(divident)<Integer.parseInt(divisor)) {
-			joiner.add(divident+"|"+divisor+"\n");
-			joiner.add(fill((divident).length(),' ')+"|");
-			joiner.add(fill((divisor).length(),'-')+"\n");
-			joiner.add(fill(divident.length(),' ')+"|"+"0\n");
+		int checkIfDivisorNegative =1;
+		int checkIfDividendNegative =1;
+		if (divisor <0) {
+			checkIfDivisorNegative=-1;
 		}
-		else{
-			joiner.add("_"+ divident+"|"+divisor+"\n");
+		if (dividend <0) {
+			checkIfDividendNegative=-1;
 		}
+		dividend = (int)Math.abs(dividend);
+		divisor = (int)Math.abs(divisor);
+		int remainder = dividend%divisor;
+		int quotient = dividend/divisor;
+		int localDividend = 0;
+		int intermediate = 0;
+		int firstIndent =0;
+		List<Step>steps = new ArrayList<>();
 		
-		boolean firstLocalDivident = false;
-		
-		int index =0;
-						
-		while (index<divident.length()) {
-			builder.append(String.valueOf(divident.charAt(index)));
-			int localDivident = Integer.parseInt(builder.toString());
-			index++;
-			if (localDivident>=Integer.parseInt(divisor)) {
-				if (firstLocalDivident) {
-					joiner.add(fill(index-String.valueOf(localDivident).length(), ' ') + "_"+String.valueOf(localDivident)+"\n");
-				}
-				int intermediate = localDivident / Integer.parseInt(divisor)*Integer.parseInt(divisor);
-				localDivident=localDivident - localDivident / Integer.parseInt(divisor)*Integer.parseInt(divisor);
-				if (!firstLocalDivident) {
-					joiner.add(fill(index-String.valueOf(intermediate).length(), ' ') + " "+ String.valueOf(intermediate));
-					joiner.add(fill(String.valueOf(divident).length()-String.valueOf(intermediate).length(), ' ')+"|");
-					if (String.valueOf(Integer.parseInt(divident)/Integer.parseInt(divisor)).length()>divisor.length()) {
-						joiner.add(fill(String.valueOf(Integer.parseInt(divident)/Integer.parseInt(divisor)).length(),'-')+"\n");
-					}else {
-						joiner.add(fill((divisor).length(),'-')+"\n");
-					}
-					
-					joiner.add(fill(index-String.valueOf(intermediate).length()+1, ' ')+fill(String.valueOf(intermediate).length(), '-'));
-					joiner.add(fill(String.valueOf(divident).length()-String.valueOf(intermediate).length(), ' ')+"|");
-					joiner.add(String.valueOf(Integer.parseInt(divident)/Integer.parseInt(divisor))+"\n");
-					firstLocalDivident = true;
+		for (int i=0;i<=findLength(quotient);i++) {
+			if (i ==0) {
+				if(findSeveralDigits(dividend,findLength(divisor))<divisor) {
+					localDividend = findSeveralDigits(dividend,findLength(divisor)+1);
+					firstIndent =findLength(localDividend);
 				}else {
-				joiner.add(fill(index-String.valueOf(intermediate).length(), ' ') + " "+ String.valueOf(intermediate)+"\n");
-				joiner.add(fill(index-String.valueOf(intermediate).length()+1, ' ')+fill(String.valueOf(intermediate).length(), '-')+"\n");
+					localDividend = findSeveralDigits(dividend,findLength(divisor));
+					firstIndent =findLength(localDividend);
 				}
-				builder.delete(0, builder.length());
-				builder.append(String.valueOf(localDivident));
+				intermediate = divisor*findDigit(quotient,0);
+			}else {
+				localDividend = (localDividend - intermediate)*10+findDigit(dividend,i+firstIndent);
+				intermediate = divisor*findDigit(quotient,i);
 			}
-				if (index==divident.length()&Integer.parseInt(divident)>=Integer.parseInt(divisor)) {
-					joiner.add(fill(index-String.valueOf(localDivident).length(), ' ')+" "+ String.valueOf(localDivident));
-			}
+			Step step = new Step(localDividend*checkIfDividendNegative, intermediate*checkIfDividendNegative);
+			steps.add(step);
 		}
-		return joiner.toString();
+		return new Result(dividend*checkIfDividendNegative,
+							divisor*checkIfDivisorNegative,
+							quotient*checkIfDividendNegative*checkIfDivisorNegative
+							,remainder*checkIfDividendNegative,
+							steps);
 	}
-	public String fill (int number,char charToFill) {
-		char [] array = new char [number];
-		Arrays.fill(array, charToFill);
-		return String.valueOf(array);
+	public static int findLength(int figure) {
+		if( figure==0) {
+			return 0;
+		}
+		return (int)(Math.log10((int)Math.abs(figure)));
+	}
+	public static int findSeveralDigits(int figure, int lastIndex) {
+
+		return figure/(int)Math.pow(10, findLength(figure)-lastIndex);
+	}
+	public static int findDigit(int figure,int index) {
+			return figure/(int)Math.pow(10, findLength(figure)-index)%10;
+
 	}
 }
+	
