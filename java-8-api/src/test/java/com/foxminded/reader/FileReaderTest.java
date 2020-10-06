216d5079b6ab0f2e8e255fe46ebc25256a766e20
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-10-06 21:18:23 +0300

    ByteArrayInputStreamTest is done

--- java-8-api/src/test/java/com/foxminded/reader/FileReaderTest.java
+++ java-8-api/src/test/java/com/foxminded/reader/FileReaderTest.java
@@ -2,13 +2,19 @@ package com.foxminded.reader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 class FileReaderTest {
-
+	
+	FileReader fr = new FileReader();
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void ByteArrayInputStreamTest() throws IOException {
+		String excpected = "test\n";
+		ByteArrayInputStream testByteArrayInputStream = new ByteArrayInputStream("test".getBytes());
+		assertEquals (excpected, fr.getBufferedString(testByteArrayInputStream));
 	}
 
 }
