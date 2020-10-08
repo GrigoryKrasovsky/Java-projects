cf4257d40b09a2a691052f590ad207b95a41290a
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-10-08 12:25:59 +0300

    getFinalMapWithMaps created

--- java-8-api/src/main/java/com/foxminded/parser/FileParser.java
+++ java-8-api/src/main/java/com/foxminded/parser/FileParser.java
@@ -25,13 +25,29 @@ public class FileParser {
 					if (i<3) {
 						abbreviation.append(line.charAt(i));
 					}else {
-						otherData.append(line.charAt(i));
+						if(i==3 && line.charAt(i)=='_') {
+						
+						}else {
+							otherData.append(line.charAt(i));
+						}	
 					}
-					
-			}
+				}
 				mapOfAbbreviations.put(abbreviation.toString(), otherData.toString());
-		}
+			}
 		return mapOfAbbreviations;
+	}
+	
+	public Map<String, Map<String, String>> getFinalMapWithMaps(){
+		Map<String, Map<String, String>> mapOfMaps = new HashMap<>();
+		Map <String, String> abbreviationsAndDataMap = getMapOfAbbreviationsAndData();
+		for(String element:abbreviationsAndDataMap.keySet()) {
+			
+			Map <String, String> insideMap = new HashMap<>();
+			insideMap.put("key0",abbreviationsAndDataMap.get(element).split("_")[0]);
+			insideMap.put("key1",abbreviationsAndDataMap.get(element).split("_")[1]);
+			mapOfMaps.put(element, insideMap);
+		}
+		return mapOfMaps;
 	}
 	
 	public String getCertainPair(String key) {
--- java-8-api/src/test/java/com/foxminded/parser/FileParserTest.java
+++ java-8-api/src/test/java/com/foxminded/parser/FileParserTest.java
@@ -9,10 +9,35 @@ import org.junit.jupiter.api.Test;
 class FileParserTest {
 	
 	@Test
-	void shouldVerifyCorrectAbbreviationReturned() throws IOException {
+	void shouldVerifyCorrectDataReturned() throws IOException {
 		FileParser fp = new FileParser("start.log");
 		String expected = "2018-05-24_12:02:58.917";
 		assertEquals(expected, fp.getCertainPair("SVF"));
+	}
+	
+	@Test
+	void shouldVerifyCorrectNameAndTeamReturned() throws IOException {
+		FileParser fp = new FileParser("abbreviations.txt");
+		String expected = "Sebastian Vettel_FERRARI";
+		assertEquals(expected, fp.getCertainPair("SVF"));
+	}
+	@Test
+	void shouldVerifyCorrectNameInFinalMapOfMaps() throws IOException {
+		FileParser fp = new FileParser("abbreviations.txt");
+		String expected = "Sebastian Vettel";
+		assertEquals(expected, fp.getFinalMapWithMaps().get("SVF").get("key0"));
+	}
+	@Test
+	void shouldVerifyCorrectTeamInFinalMapOfMaps() throws IOException {
+		FileParser fp = new FileParser("abbreviations.txt");
+		String expected = "RED BULL RACING TAG HEUER";
+		assertEquals(expected, fp.getFinalMapWithMaps().get("DRR").get("key1"));
+	}
+	@Test
+	void shouldVerifyCorrectTimeInFinalMapOfMaps() throws IOException {
+		FileParser fp = new FileParser("start.log");
+		String expected = "12:14:12.054";
+		assertEquals(expected, fp.getFinalMapWithMaps().get("DRR").get("key1"));
 	}
 
 }
