0f74de7fe644b79b45431898ba190a25b5ea0483
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-10-09 20:31:19 +0300

    parseRacersInfo created

--- java-8-api/src/main/java/com/foxminded/parser/FileParser.java
+++ java-8-api/src/main/java/com/foxminded/parser/FileParser.java
@@ -1,73 +1,35 @@
 package com.foxminded.parser;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.foxminded.model.Racer;
-import com.foxminded.model.Racers;
-import com.foxminded.reader.FileReader;
-
 public class FileParser {
-	private final String outputString;
-	Stream<String> outputStream;
-	public FileParser(String fileName) throws IOException, URISyntaxException {
-		FileReader fr = new FileReader();
-		this.outputStream = fr.getStreamFromFile(fileName);
-		this.outputString = fr.getBufferedString(fr.getInputStream(fileName));
-		
-	}
-	public Map<String, String> getMapOfAbbreviationsAndData () {
-		String[] lines = outputString.split("\n");
-		Map<String, String>mapOfAbbreviations = new HashMap<>();
-		for (String line:lines) {
-			StringBuilder abbreviation = new StringBuilder();
-			StringBuilder otherData = new StringBuilder();
-				for (int i=0; i<line.length();i++) {
-					if (i<3) {
-						abbreviation.append(line.charAt(i));
-					}else {
-						if(i==3 && line.charAt(i)=='_') {
-						
-						}else {
-							otherData.append(line.charAt(i));
-						}	
-					}
-				}
-				mapOfAbbreviations.put(abbreviation.toString(), otherData.toString());
-			}
-		return mapOfAbbreviations;
+	
+	private static final DateTimeFormatter DATETIME_FORMAT = 
+			DateTimeFormatter.ofPattern("uuuu-MM-dd_HH:mm:ss.SSS", Locale.ENGLISH);
+	
+	private final Path path;
+	
+	public FileParser(Path path) {
+		this.path = path;	
 	}
 	
-	public Map<String, Map<String, String>> getMapWithMaps(){
-		Map<String, Map<String, String>> mapOfMaps = new HashMap<>();
-		Map <String, String> abbreviationsAndDataMap = getMapOfAbbreviationsAndData();
-		for(String element:abbreviationsAndDataMap.keySet()) {
-			
-			Map <String, String> insideMap = new HashMap<>();
-			insideMap.put("key0",abbreviationsAndDataMap.get(element).split("_")[0]);
-			insideMap.put("key1",abbreviationsAndDataMap.get(element).split("_")[1]);
-			mapOfMaps.put(element, insideMap);
+	public Map <Object, Object> parseRacersInfo() throws IOException {
+		try (Stream<String>lines = Files.lines(path)){
+			return lines.collect(Collectors.toMap(
+					string -> string.substring(0,3),
+					string -> Arrays.asList(string.substring(3).split("_"))));
 		}
-		return mapOfMaps;
 	}
-
-	public String getCertainPair(String key) {
-		return getMapOfAbbreviationsAndData().get(key);
-	}
-	
-	public List<String> getRacersAbbreviations(){
-		Racers racers = new Racers();
-		return outputStream.map(line -> Arrays.asList(line.split("_")))
-				.map(element -> element.get(0))
-				.map(string -> string.substring(0,3)).collect(Collectors.toList());
-	}
-	//return outputStream.map(racers.add(new Racer("","","","","","")));
-	
 }
+		
+
--- java-8-api/src/test/java/com/foxminded/parser/FileParserTest.java
+++ java-8-api/src/test/java/com/foxminded/parser/FileParserTest.java
@@ -4,55 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
 class FileParserTest {
 	
 	@Test
-	void shouldVerifyCorrectDataReturned() throws IOException, URISyntaxException {
-		FileParser fp = new FileParser("start.log");
-		String expected = "2018-05-24_12:02:58.917";
-		assertEquals(expected, fp.getCertainPair("SVF"));
-	}
-	
-	@Test
-	void shouldVerifyCorrectNameAndTeamReturned() throws IOException, URISyntaxException {
-		FileParser fp = new FileParser("abbreviations.txt");
-		String expected = "Sebastian Vettel_FERRARI";
-		assertEquals(expected, fp.getCertainPair("SVF"));
-	}
-	@Test
-	void shouldVerifyCorrectNameInMapOfMaps() throws IOException, URISyntaxException {
-		FileParser fp = new FileParser("abbreviations.txt");
-		String expected = "Sebastian Vettel";
-		assertEquals(expected, fp.getMapWithMaps().get("SVF").get("key0"));
-	}
-	@Test
-	void shouldVerifyCorrectTeamInMapOfMaps() throws IOException, URISyntaxException {
-		FileParser fp = new FileParser("abbreviations.txt");
-		String expected = "RED BULL RACING TAG HEUER";
-		assertEquals(expected, fp.getMapWithMaps().get("DRR").get("key1"));
-	}
-	@Test
-	void shouldVerifyCorrectTimeInMapOfMaps() throws IOException, URISyntaxException {
-		FileParser fp = new FileParser("start.log");
+	void shouldVerifyCorrectgetRacersOutput() throws IOException, URISyntaxException {
+		
+		Path path = Paths.get(getClass().getClassLoader()
+			      .getResource("start.log").toURI());
+		FileParser fp = new FileParser(path);
 		String expected = "12:14:12.054";
-		assertEquals(expected, fp.getMapWithMaps().get("DRR").get("key1"));
-	}
-	
-	@Test
-	void shouldVerifyCorrectStreamOutput() throws IOException, URISyntaxException {
-		FileParser fp = new FileParser("start.log");
-		String expected = "12:14:12.054";
-		assertEquals(expected, fp.getRacersAbbreviations());
-	}
-	
-	@Test
-	void shouldVerifyCorrectgetRacersAbbreviations() throws IOException, URISyntaxException {
-		FileParser fp = new FileParser("abbreviations.txt");
-		String expected = "12:14:12.054";
-		assertEquals(expected, fp.getRacersAbbreviations());
+		assertEquals(expected, fp.parseRacersInfo());
 	}
 
 }
