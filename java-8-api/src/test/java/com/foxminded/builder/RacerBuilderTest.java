edfe52f1ea80986299f3bda8a7fde715e7b42fbc
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-10-11 23:15:43 +0300

    GeneralRacerBuilderTestForRacer10 created

--- java-8-api/src/test/java/com/foxminded/builder/RacerBuilderTest.java
+++ java-8-api/src/test/java/com/foxminded/builder/RacerBuilderTest.java
@@ -4,6 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,9 +21,16 @@ class RacerBuilderTest {
 		assertEquals(expected, racerBuilder.buildRacers().get(10).getAbbreviation());
 	}
 	@Test
-	void shouldVerifyCorrectGeneralInfoReturned() throws URISyntaxException, IOException {
+	void shouldVerifyCorrectGeneralInfoReturnedForRacerTen() throws URISyntaxException, IOException {
 		RacerBuilder racerBuilder = new RacerBuilder();
-		String expected = "LSW";
+		Map<Object, Object> expected = Stream.of(
+				  new AbstractMap.SimpleEntry<>("abbreviation", "LSW"),
+				  new AbstractMap.SimpleEntry<>("name", "Lance Stroll"), 
+				  new AbstractMap.SimpleEntry<>("team", "WILLIAMS MERCEDES"), 
+				  new AbstractMap.SimpleEntry<>("startTime", "12:06:13.511"), 
+				  new AbstractMap.SimpleEntry<>("endTime", "12:07:26.834"), 
+				  new AbstractMap.SimpleEntry<>("date", "2018-05-24"))
+				  .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 		assertEquals(expected, racerBuilder.buildRacers().get(10).toMap());
 	}
 }
