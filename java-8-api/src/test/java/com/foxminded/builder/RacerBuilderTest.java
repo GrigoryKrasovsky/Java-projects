34cb6d928c3c4ff0c995a3f5fed7f4ba0e591538
Grigory Krasovsky <iceteagreen1@gmail.com>
2020-10-14 18:17:44 +0300

    RacerBuilder test complete

--- java-8-api/src/test/java/com/foxminded/builder/RacerBuilderTest.java
+++ java-8-api/src/test/java/com/foxminded/builder/RacerBuilderTest.java
@@ -26,28 +26,13 @@ class RacerBuilderTest {
 		assertEquals(expected, racerBuilder.buildRacers().get(10).getAbbreviation());
 	}
 	@Test
-	void shouldVerifyCorrectGeneralInfoReturnedForRacerTen() throws URISyntaxException, IOException {
+	void shouldVerifyCorrectInfoReturnedForRacerTen() throws URISyntaxException, IOException {
 		RacerBuilder racerBuilder = new RacerBuilder();
-		
-		LocalTime startTime = LocalTime.from(DateTimeFormatter.ofPattern("HH:mm:ss.SSS").parse("12:06:13.511"));
-		LocalTime endTime = LocalTime.from(DateTimeFormatter.ofPattern("HH:mm:ss.SSS").parse("12:07:26.834"));
-		LocalDate localDate = LocalDate.parse("2018-05-24", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-		
-		Map<Object, Object> expected = Stream.of(
-				  new AbstractMap.SimpleEntry<>("abbreviation", "LSW"),
-				  new AbstractMap.SimpleEntry<>("name", "Lance Stroll"), 
-				  new AbstractMap.SimpleEntry<>("team", "WILLIAMS MERCEDES"), 
-				  new AbstractMap.SimpleEntry<>("startTime", startTime), 
-				  new AbstractMap.SimpleEntry<>("endTime", endTime), 
-				  new AbstractMap.SimpleEntry<>("date", localDate))
-				  .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
 		assertEquals("LSW", racerBuilder.buildRacers().get(10).getAbbreviation());
-		assertEquals(2018-05-24, racerBuilder.buildRacers().get(10).getDate());
-		assertEquals(2, racerBuilder.buildRacers().get(10).getStartTime());
-		assertEquals(3, racerBuilder.buildRacers().get(10).getEndTime());
-		assertEquals(3, racerBuilder.buildRacers().get(10).getTeam());
-
-		
-
+		assertEquals("2018-05-24", racerBuilder.buildRacers().get(10).getDate().toString());
+		assertEquals("12:06:13.511", racerBuilder.buildRacers().get(10).getStartTime().toString());
+		assertEquals("12:07:26.834", racerBuilder.buildRacers().get(10).getEndTime().toString());
+		assertEquals("WILLIAMS MERCEDES", racerBuilder.buildRacers().get(10).getTeam().toString());
 	}
 }
